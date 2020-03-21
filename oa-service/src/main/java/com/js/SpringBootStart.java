776133ccad0bab71638235812f4c8e759cdbbfed
姜爽 <jiangshuang@suixingpay.com>
2020-03-21 13:52:22 +0800

    黑大OA底层架构

---
+++ oa-service/src/main/java/com/js/SpringBootStart.java
@@ -0,0 +1,50 @@
+package com.js;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 启动类
+ * @Author jishubu
+ */
+@SpringBootApplication
+@Slf4j
+@MapperScan("com.js.mapper")
+public class SpringBootStart {
+    public static void main(String[] args) {
+        try{
+            SpringApplication.run(SpringBootStart.class,args);
+            log.info(print());
+            log.info("项目启动成功");
+        }catch (Exception e){
+            log.info("项目启动失败");
+        }
+
+    }
+    private static String print() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("\n");
+        sb.append("                   _ooOoo_\n");
+        sb.append("                  o8888888o\n");
+        sb.append("                  88\" . \"88\n");
+        sb.append("                  (| -_- |)\n");
+        sb.append("                  O\\  =  /O\n");
+        sb.append("               ____/`---'\\____\n");
+        sb.append("             .'  \\\\|     |//  `.\n");
+        sb.append("            /  \\\\|||  :  |||//  \\ \n");
+        sb.append("           /  _||||| -:- |||||-  \\ \n");
+        sb.append("           |   | \\\\\\  -  /// |   |\n");
+        sb.append("           | \\_|  ''\\---/''  |   |\n");
+        sb.append("           \\  .-\\__  `-`  ___/-. /\n");
+        sb.append("         ___`. .'  /--.--\\  `. . __\n");
+        sb.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
+        sb.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
+        sb.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
+        sb.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
+        sb.append("                   `=---='\n");
+        sb.append("...................................................\n");
+        return sb.toString();
+    }
+}
