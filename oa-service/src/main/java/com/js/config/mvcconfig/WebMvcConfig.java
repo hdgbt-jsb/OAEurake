59be0956721eeda528ea778db3e778064ba0b5c2
姜爽 <jiangshuang@suixingpay.com>
2020-03-27 19:39:14 +0800

    将所有不拦截的路径整理为List集合

--- oa-service/src/main/java/com/js/config/mvcconfig/WebMvcConfig.java
+++ oa-service/src/main/java/com/js/config/mvcconfig/WebMvcConfig.java
@@ -5,6 +5,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Author jiangshuang
  * @Description 拦截未登录的操作
@@ -15,9 +19,16 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Autowired
     private MyHandlerIntercepter myHandlerIntercepter;
 
+    /**
+     * 不拦截路径
+     **/
+    private List<String> excludePathPatterns = new ArrayList<>();
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(myHandlerIntercepter).addPathPatterns("/**").excludePathPatterns("/userlogin");
+        excludePathPatterns.add("/swagger-ui.html");
+        excludePathPatterns.add("/v2/api-docs");
+        registry.addInterceptor(myHandlerIntercepter).addPathPatterns("/**").excludePathPatterns(excludePathPatterns);
     }
 
 }
