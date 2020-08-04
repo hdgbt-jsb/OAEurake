6e7af8e829a63ddba70d3c0230e5ec5e8d22b545
姜爽 <2545251075@qq.com>
2020-08-04 13:47:50 +0800

    fix

---
+++ oa-gateway/src/main/java/com/js/OaGateWayStart.java
@@ -0,0 +1,51 @@
+package com.js;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description 网关启动类
+ * @Author jishubu
+ */
+@Slf4j
+@EnableDiscoveryClient
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+public class OaGateWayStart {
+    public static void main(String[] args) {
+        try{
+            SpringApplication.run(OaGateWayStart.class,args);
+            log.info(print());
+            log.info("项目启动成功");
+        }catch (Exception e){
+            log.info("项目启动失败");
+        }
+
+    }
+    private static synchronized String print() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\n");
+        sb.append("                   _ooOoo_\n");
+        sb.append("                  o8888888o\n");
+        sb.append("                  88\" . \"88\n");
+        sb.append("                  (| -_- |)\n");
+        sb.append("                  O\\  =  /O\n");
+        sb.append("               ____/`---'\\____\n");
+        sb.append("             .'  \\\\|     |//  `.\n");
+        sb.append("            /  \\\\|||  :  |||//  \\ \n");
+        sb.append("           /  _||||| -:- |||||-  \\ \n");
+        sb.append("           |   | \\\\\\  -  /// |   |\n");
+        sb.append("           | \\_|  ''\\---/''  |   |\n");
+        sb.append("           \\  .-\\__  `-`  ___/-. /\n");
+        sb.append("         ___`. .'  /--.--\\  `. . __\n");
+        sb.append("      .\"\" '<  `.___\\_<|>_/___.'  >'\"\".\n");
+        sb.append("     | | :  `- \\`.;`\\ _ /`;.`/ - ` : | |\n");
+        sb.append("     \\  \\ `-.   \\_ __\\ /__ _/   .-` /  /\n");
+        sb.append("======`-.____`-.___\\_____/___.-`____.-'======\n");
+        sb.append("                   `=---='\n");
+        sb.append("...................................................\n");
+        return sb.toString();
+    }
+}
